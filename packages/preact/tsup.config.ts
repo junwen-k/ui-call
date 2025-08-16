b1960c69f0a4e56852cd8b09fca5bb799d0125cf
junwen-k <40173716+junwen-k@users.noreply.github.com>
2025-08-16 16:38:17 +0800

    feat(packages): support cjs format

--- packages/preact/tsup.config.ts
+++ packages/preact/tsup.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'tsup';
 
 export default defineConfig({
   entry: ['src/index.ts', 'src/signals/index.ts'],
-  format: ['esm'],
+  format: ['cjs', 'esm'],
   dts: true,
 });
